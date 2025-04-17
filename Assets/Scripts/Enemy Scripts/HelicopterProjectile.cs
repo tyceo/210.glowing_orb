efb9473c74eece0775a1ba4c37540ec1bfdcb195
HenryVDB123 <1033595@student.sae.edu.au>
2025-04-17 14:01:14 +1000

    Enemy models and projectile fixes

--- Assets/Scripts/Enemy Scripts/HelicopterProjectile.cs
+++ Assets/Scripts/Enemy Scripts/HelicopterProjectile.cs
@@ -5,6 +5,7 @@ public class HelicopterProjectile : MonoBehaviour
     //Delcare Variables
     [SerializeField] private float moveSpeed;
     public float damage;
+    [SerializeField] private float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,9 +16,32 @@ public class HelicopterProjectile : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Travel Forward
-        //transform.position += transform.forward * moveSpeed;
-        GetComponent<Rigidbody>().linearVelocity = (transform.forward * moveSpeed);
+ 
+        transform.position += transform.forward * moveSpeed;
+
     }
 
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("PlayerProjectile"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/Enemy Scripts/TurretProjectile.cs
+++ Assets/Scripts/Enemy Scripts/TurretProjectile.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 
 public class TurretProjectile : MonoBehaviour
@@ -5,6 +6,7 @@ public class TurretProjectile : MonoBehaviour
     //Delcare Variables
     [SerializeField] private float moveSpeed;
     public float damage;
+    [SerializeField] private float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,5 +20,30 @@ public class TurretProjectile : MonoBehaviour
         //Travel Forward
         //transform.position += transform.forward * moveSpeed;
         GetComponent<Rigidbody>().linearVelocity = (transform.forward * moveSpeed);
+
+        
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 6)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("PlayerProjectile"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
